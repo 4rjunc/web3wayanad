36676d0f5f3eb02eee6e00dee34d883bbd08073c
4rjunc <arjunmundamani@gmail.com>
2025-03-15 12:07:44 +0530

    Changed ts-node to tsx

--- keygen.ts
+++ keygen.ts
@@ -2,6 +2,7 @@ import { Keypair } from "@solana/web3.js";
 
 //Generate a new keypair
 
+
 console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}
 
 To save your wallet, copy and paste the following into a JSON file:
